cb59172119af2bbbd6f9b5b571c9b1accc734207
Shubham2612-ncc <shubhu123007@gmail.com>
2024-12-12 13:01:01 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Answer from './components/Person';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello> </Hello>
+        <Answer name="Shubham" place="India"/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Person.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Answer =({name, place})=>{
+    return (
+        <h1>Hello from {name}, I'm from {place}</h1>
+    )
+}
+
+export default Answer;
